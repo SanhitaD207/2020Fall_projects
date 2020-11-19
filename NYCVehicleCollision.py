fd2bd08112fcc3b3bf7465ba460a6045ba324319
Sanhita207 <sanhita.dhamdhere5@gmail.com>
2020-11-19 17:37:15 -0600

    Data extracted for Hypothesis 2

--- NYCVehicleCollision.py
+++ NYCVehicleCollision.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime, time
 
 
 class NYCMotorVehicleCollisions:
@@ -8,12 +9,28 @@ class NYCMotorVehicleCollisions:
         self.persons = {}
         self.vehicles = {}
 
-
     def _load_dataframes(self):
         self.crashes = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv', low_memory=False)
-        self.persons = pd.read_csv('Motor_Vehicle_Collisions_-_Person.csv', low_memory=False)
-        self.vehicles = pd.read_csv('Motor_Vehicle_Collisions_-_Vehicles.csv', low_memory=False)
+        # self.persons = pd.read_csv('Motor_Vehicle_Collisions_-_Person.csv', low_memory=False)
+        # self.vehicles = pd.read_csv('Motor_Vehicle_Collisions_-_Vehicles.csv', low_memory=False)
 
-        print('Crashes df shape ', self.crashes.shape)
-        print('Persons df shape ', self.persons.shape)
-        print('Vehicles df shape ', self.vehicles.shape)
+        # print('Crashes df shape ', self.crashes)
+        # print('Persons df shape ', self.persons)
+        # print('Vehicles df shape ', self.vehicles.shape)
+        self.night_crashes_analysis()
+
+    def night_crashes_analysis(self):
+        print('Crashes df shape ', self.crashes)
+        time_data = self.crashes['CRASH TIME']
+        print(time_data)
+
+        self.crashes['CRASH TIME'] = self.crashes['CRASH TIME'].apply(lambda x: datetime.strptime(x, "%H:%M").time())
+
+        night_crashes_analysis = self.crashes[(self.crashes['CRASH TIME'] < time(5, 0, 0))]
+
+        print('Crashes df shape ', self.crashes)
+        print(night_crashes_analysis)
+
+
+collision_data = NYCMotorVehicleCollisions()
+collision_data._load_dataframes()
