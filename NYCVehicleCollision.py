3e7dcabcfc43fc862b76cb3e49f624761f80acae
Adarsh Agarwal <agarwaladarsh95@gmail.com>
2020-11-18 23:30:34 -0600

    Class structure setup, along with init and load functions

--- NYCVehicleCollision.py
+++ NYCVehicleCollision.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
-data1 = pd.read_excel("MVCollisionsDataDictionary_20190813_ERD.xlsx", skiprows=15)
 
-print(data1[0,:])
+class NYCMotorVehicleCollisions:
+
+    def __init__(self):
+        self.crashes = {}
+        self.persons = {}
+        self.vehicles = {}
+
+
+    def _load_dataframes(self):
+        self.crashes = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv', low_memory=False)
+        self.persons = pd.read_csv('Motor_Vehicle_Collisions_-_Person.csv', low_memory=False)
+        self.vehicles = pd.read_csv('Motor_Vehicle_Collisions_-_Vehicles.csv', low_memory=False)
+
+        print('Crashes df shape ', self.crashes.shape)
+        print('Persons df shape ', self.persons.shape)
+        print('Vehicles df shape ', self.vehicles.shape)
